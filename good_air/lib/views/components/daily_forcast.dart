a3fff7a907236a05c9b4eaabd7cbf08ba3f5dd69
Simone <simone.orlando@mail.polimi.it>
2020-12-06 18:15:35 +0100

    added the forecasts without graphics

--- good_air/lib/views/components/daily_forcast.dart
+++ good_air/lib/views/components/daily_forcast.dart
@@ -25,6 +25,16 @@ class DailyForecast extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
      var size = MediaQuery.of(context).size;
+     var color_left = Colors.black;
+     var color_right = Colors.black;
+     var iconSwipeLeft;
+     var iconSwipeRight;
+     if(this.index == 0)
+       color_left = Colors.white;
+     if(this.index == daily.o3.length - 1)
+       color_right = Colors.white;
+     iconSwipeLeft = Icon(Icons.arrow_left, color: color_left,);
+     iconSwipeRight = Icon(Icons.arrow_right, color: color_right); 
      return Container(
       margin: EdgeInsets.only(left: size.width*0.03, top: size.height*0.1, right: size.width*0.03, bottom: size.height*0.02),
       height: double.infinity,
@@ -48,7 +58,7 @@ class DailyForecast extends StatelessWidget {
       ),
       child: Column(
         children: [
-          Text(day_pm25.day),
+          Row(mainAxisAlignment: MainAxisAlignment.center,children: [iconSwipeLeft,Text(day_pm25.day),iconSwipeRight]),
           Text("O3: " + "min: " + day_o3.min.toString() + " max: " + day_o3.max.toString() + " avg: " + day_o3.avg.toString()),
           Text("Pm10: " + "min: " + day_pm10.min.toString() + " max: " + day_pm10.max.toString() + " avg: " + day_pm10.avg.toString()),
           Text("Pm25: " + "min: " + day_pm25.min.toString() + " max: " + day_pm25.max.toString() + " avg: " + day_pm25.avg.toString()),
